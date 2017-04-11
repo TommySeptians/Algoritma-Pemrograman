180fd145a94a88e53b8ff621d5f22811a37d6498
TommySeptians <tomprinsheart@gmail.com>
2017-04-11 21:58:58 +0700

    Update Tugas 5 - Array.cpp

--- Tugas 5 - Array.cpp
+++ Tugas 5 - Array.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 
 int main () {
-	int A = 0;
+	int x, A = 0;
 	char karakter [10];
-	for (int x = 0; x < 10; x++) {
+	for (x = 0; x < 10; x++) {
 		cout << "Masukkan Karakter : ";
 		cin >> karakter [x];
 		if (karakter [x] == 'A') {
 			A++;
 		}
 	}
-	cout << "Banyaknya Elemen dengan Karakter A : " << A;
+	for (x = 0; x < 10; x++) {
+		cout << "\nKarakter Ke-" << x+1 << "\t : ";
+		cout << karakter [x];
+	}
+	cout << "\nBanyaknya Elemen dengan Karakter A : " << A;
 	return 0;
 }
