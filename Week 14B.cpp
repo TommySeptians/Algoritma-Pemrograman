504538fc9ac124abe0f0ad3fa8ff52040fbfb42b
TommySeptians <tomprinsheart@gmail.com>
2017-06-06 19:45:25 +0700

    Add files via upload

---
+++ Week 14B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void numberFunction (int i) {
+	cout << "The number is: " << i << endl;
+	i++; // pengganti
+	if (i < 10) {
+		numberFunction (i);
+	}
+}
+
+int main () {
+	int i = 0;
+	numberFunction (i);
+	return 0;
+}
